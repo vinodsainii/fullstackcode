2cce9a2a2c6f651c5c95218f0ba0c0f54ec21ec8
vinodsainii <vs0761812@gmail.com>
2021-06-30 14:24:49 +0530

    test

---
+++ test/assesment2/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './App.css';
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+
+
+function App() {
+  return (
+    <div>
+       <Nav />
+       <Footer />
+    </div>
+  );
+}
+
+export default App;
