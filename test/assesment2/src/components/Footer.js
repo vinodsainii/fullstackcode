e9354f5d843486c28b621894b063b9d07ad2f4f3
vinodsainii <vs0761812@gmail.com>
2021-06-30 14:25:28 +0530

    test

---
+++ test/assesment2/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import '../component/footer.css'
+
+const Footer = () => {
+  return (
+    <div className="footer">
+    <footer>
+      <div className="left">
+        <h1>Footer content</h1> 
+      </div>
+      <div className="right">
+        <a href="#">link</a>
+      </div>
+    </footer>
+    </div>
+  );
+}
+
+export default Footer;
